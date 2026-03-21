09206d70a8d04e4b9825031af76c946f6decf37c
JJJ-un <jhjk7272@naver.com>
2026-03-21 14:34:54 +0900

    feat: 보로노이 트리맵 기본 구현 및 Vite/TS 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
+  server: {
+    proxy: {
+      // 해당 부분 추후 API 주소에 맞춰 수정해야함
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 })
